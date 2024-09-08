e25b60c412bdacce444134983bf58fcfa74fa6d8
Marat2010 <marat.s.73@mail.ru>
2024-09-08 15:03:11 +0300

    edit bot config

---
+++ our_Bots/bot_15001/Run_bot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#=========================================
+#Запуск бота:
+#=========================================
+
+cd "$HOME/$PROJECT_NAME" || { echo "----- !!!!! Ошибка !!!!! -----"; }
+source .venv/bin/activate
+
+python ./our_Bots/bot_15001/bot_1.py 
+
+
+#=========================================
+#echo "=== Запуск сервиса, службы (SYSTEMD) бота ===" 
+#echo
+#sudo cp /$HOME/$PROJECT_NAME/our_Bots/bot1/bot_1.service /lib/systemd/system/bot_1.service
+#sudo systemctl daemon-reload
+#sudo systemctl enable bot_1.service
+#sudo systemctl start bot_1.service
+#=========================================
+#cd /home/marat/PycharmProjects/manageBots/
+#=========================================
+
